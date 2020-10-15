6ba5b91b1b0d4d5c186e9e466223dfd5675c98d7
lauren <ljdutton2@icloud.com>
2020-10-15 17:53:16 -0500

    frontend

---
+++ scripts/issue-token.js
@@ -0,0 +1,8 @@
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(callback) {
+    console.log('Issued Tokens!!')
+    let tokenFarm = await TokenFarm.deployed()
+    await tokenFarm.issueTokens()
+    callback()
+};
