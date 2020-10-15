f4d5aab75a73ef184c977a4d32bfd820bfe3355f
lauren <ljdutton2@icloud.com>
2020-10-15 12:41:22 -0500

    deployed

--- src/contracts/KarmaToken.sol
+++ src/contracts/KarmaToken.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
-contract DappToken {
-    string  public name = "DApp Token";
-    string  public symbol = "DAPP";
+contract KarmaToken {
+    string  public name = "Karma";
+    string  public symbol = "K";
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8   public decimals = 18;
 
---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+import './KarmaToken.sol';
+import './DaiToken.sol';
+
+contract TokenFarm {
+
+string public name = 'Karma Token Farm';
+KarmaToken public karmaToken
+DaiToken public daiToken
+
+constructor(KarmaToken _karmaToken, DaiToken _daiToken) public {
+
+    karmaToken = _karmaToken;
+    daiToken = _daiToken;
+
+}
+
+
+}
