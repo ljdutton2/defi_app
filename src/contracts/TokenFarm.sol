037c48fa94483147a3dc8e3e43615f0784f1add9
lauren <ljdutton2@icloud.com>
2020-10-15 14:41:53 -0500

    ability to stake tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,11 +8,29 @@ string public name = 'Karma Token Farm';
 KarmaToken public karmaToken;
 DaiToken public daiToken;
 
+address[] public stakers
+mapping(address => uint) public stakingBalance)
+mapping(address => bool) public hasStaked
+mapping(address => bool) public isStaking
 constructor(KarmaToken _karmaToken, DaiToken _daiToken) public {
-
     karmaToken = _karmaToken;
     daiToken = _daiToken;
 
+}
+
+function stakeTokens(uint _amount) public {
+    // transfer the mock tokens to the contract
+    daiToken.transferFrom(msg.sender,address(this), _amount);
+
+    //update their staking balance
+    stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount
+
+    // add user to staking array if they have not previously
+    if(!hasStaked[msg.sender]) {
+        stakers.push(msg.sender;)
+    }
+    //update their status
+    hasStaked[msg.sender] = true;
 }
 
 
